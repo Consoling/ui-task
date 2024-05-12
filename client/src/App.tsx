a259ddf7eebc757e7383bf8ee751f241df4bd916
Ariz Shahid <81011174+Consoling@users.noreply.github.com>
2024-05-12 23:33:54 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,9 @@ import ControlPanel from "./components/control-panel";
 // import PanelComp from "./components/panel";
 import ResizablePanel from "./components/resizable-panel";
 
-type Props = {};
 
-const App = (props: Props) => {
+
+const App = () => {
   return (
     <div className="">
     <ControlPanel />
